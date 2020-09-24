276d084e8ee7e550e23c21a982fa0f0551718431
Kareem <k2gees@gmail.com>
2020-09-24 08:04:02 -0500

    multi-line strings

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 console.log(profileDataArgs);
 
+const generatePage = (userName, githubName) => {
+    return `
+    Name: ${userName} 
+    Github: ${githubName}
+    `;
+};
+console.log(generatePage('Jane', 'janehub'));
 
 
 
@@ -35,13 +42,13 @@ console.log(profileDataArgs);
 
 
 
-const printProfileData = profileDataArr => {
-    for (let i=0; i < profileDataArr.length; i +=1) {
-        console.log(profileDataArr[i]);
-    } 
-    console.log('===================');
+// const printProfileData = profileDataArr => {
+//     for (let i=0; i < profileDataArr.length; i +=1) {
+//         console.log(profileDataArr[i]);
+//     } 
+//     console.log('===================');
     
-    profileDataArr.forEach(profileItem) => console.log(profileItem));
-}
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// }
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
